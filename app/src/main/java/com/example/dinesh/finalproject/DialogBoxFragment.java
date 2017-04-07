efaa1ca4980c82bb51e2571ac6c0bd76a34295a9
vbarot1 <vbarot1@student.gsu.edu>
2017-04-07 14:05:57 -0400

    Update

---
+++ app/src/main/java/com/example/dinesh/finalproject/DialogBoxFragment.java
@@ -0,0 +1,26 @@
+package com.example.dinesh.finalproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class DialogBoxFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dialog_box, container, false);
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/dinesh/finalproject/ListViewFragment.java
@@ -0,0 +1,38 @@
+package com.example.dinesh.finalproject;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        //cals function, which goes to function, that shows elements want to display, set up adapter, and use ListView
+        View mView = inflater.inflate(R.layout.fragment_list_view, container, false);
+
+
+        String[] items = {"One", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "11", "12" +
+                "one", "one","one","one" };
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, items);
+
+        ListView lv = (ListView) mView.findViewById(R.id.listView);
+        lv.setAdapter(itemsAdapter);
+
+
+
+        return mView;
+
+    }
+
+
+}
