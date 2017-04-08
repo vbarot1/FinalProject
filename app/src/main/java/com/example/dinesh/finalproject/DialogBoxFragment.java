5a444874d783b22eda07e65f2aec4a80441adbb5
vbarot1 <vbarot1@student.gsu.edu>
2017-04-08 19:52:21 -0400

    Update

--- app/src/main/java/com/example/dinesh/finalproject/DialogBoxFragment.java
+++ app/src/main/java/com/example/dinesh/finalproject/DialogBoxFragment.java
@@ -5,27 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-
 
 public class DialogBoxFragment extends Fragment {
 
-    Button mShowDialog;
+   /* final Context context = this;
+    private Button button;*/
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-    mShowDialog = (Button) this.getActivity().findViewById(R.id.btnShowDialog);
-
-
-        // Inflate the layout for this fragment
+      /*  // Inflate the layout for this fragment
+        getDialog().setTitle("Simple Dialog");*/
         return inflater.inflate(R.layout.fragment_dialog_box, container, false);
     }
-
-
-
-
 }
