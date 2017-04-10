4eec566fd5c5dd5a81e3d0dca2c6a4d8cab0bf47
vbarot1 <vbarot1@student.gsu.edu>
2017-04-09 20:28:06 -0400

    Update

--- app/src/main/java/com/example/dinesh/finalproject/DialogBoxFragment.java
+++ app/src/main/java/com/example/dinesh/finalproject/DialogBoxFragment.java
@@ -1,31 +1,53 @@
 package com.example.dinesh.finalproject;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+public class DialogBoxFragment extends DialogFragment {
+
+    Button btn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
-public class DialogBoxFragment extends Fragment {
 
-    Button mShowDialog;
+    }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+       final FragmentManager fm = getFragmentManager();
+
+
+        btn = (Button) view.findViewById(R.id.btnShowDialog);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+                alertDialogBuilder.setMessage("This is the pop button that appears when 'Show Dialog' is clicked");
+                alertDialogBuilder.show();
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+      /*  // Inflate the layout for this fragment
+        getDialog().setTitle("Simple Dialog");*/
 
-        mShowDialog = (Button) this.getActivity().findViewById(R.id.btnShowDialog);
 
 
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dialog_box, container, false);
     }
-
-
-
-
 }
