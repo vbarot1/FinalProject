85945df53e972dc590e22796e2672d73f7f787c4
vbarot1 <vbarot1@student.gsu.edu>
2017-04-10 21:18:54 -0400

    Update

--- app/src/main/java/com/example/dinesh/finalproject/ListViewFragment.java
+++ app/src/main/java/com/example/dinesh/finalproject/ListViewFragment.java
@@ -20,7 +20,11 @@ public class ListViewFragment extends Fragment {
 
 
         String[] items = {"One", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve",
-                "thirteen", "fourteen","fifteen","sixteen", "seventeen", "eighteen", "nineteen", "twenty" };
+                "thirteen", "fourteen","fifteen","sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two",
+                "twenty-three", "twenty-four", "twenty-five", "twenty-six",
+        "twenty-seven", "twenty-eight", "twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five",
+        "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four",
+        "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty"};
 
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, android.R.id.text1, items);
